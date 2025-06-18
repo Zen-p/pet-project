b1d88a607eee76686fe24fc1b7b716f4df4c1609
evgenijpesko <peshkozena66@gmail.com>
2025-06-18 15:44:48 +0300

    first commit. Initialized first demo structure for future development

---
+++ Order-Service/src/main/java/ru/youdzhin/models/Order.java
@@ -0,0 +1,19 @@
+package ru.youdzhin.models;
+
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "\"order\"")
+public class Order {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+
+
+}
