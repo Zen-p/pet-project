a9cad3a620d4a19597a09862c966496238b38ef0
evgenijpesko <peshkozena66@gmail.com>
2025-06-28 13:58:10 +0300

    see reaction o pull request

--- auth-service/src/main/java/org/youdzhin/auth/AuthServiceApplication.java
+++ auth-service/src/main/java/org/youdzhin/auth/AuthServiceApplication.java
@@ -1,7 +1,6 @@
 package org.youdzhin.auth;
 
 
-import lombok.RequiredArgsConstructor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.http.ResponseEntity;
